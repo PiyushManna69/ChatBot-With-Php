3ec3b6e7507bce3d5ec2f3cd0420271f3e91b0f0
Piyush Manna <piyushmanna69@gmail.com>
2023-11-04 17:14:53 +0545

    Update process_message.php

--- process_message.php
+++ process_message.php
@@ -4,10 +4,8 @@ require 'database.php';
 if (isset($_POST['message'])) {
     $message = $_POST['message'];
 
-    // Escape the user input to prevent SQL injection
+  
     $message = $conn->real_escape_string($message);
-
-    // Perform a partial match using LIKE
     $sql = "SELECT answer FROM chat_data WHERE question LIKE '%" . $message . "%' LIMIT 1";
 
     $result = $conn->query($sql);
